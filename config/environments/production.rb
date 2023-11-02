e18de241a5811a000dc306dc1afc87633e5254cf
WTNB1225 <144193938+WTNB1225@users.noreply.github.com>
2023-11-02 12:00:41 +0900

    Add password reset

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :smpt
+  config.action_mailer.delivery_method = :smtp
   host = 'rails-u6r2.onrender.com'
   config.action_mailer.default_url_options = {host:host}
   ActionMailer::Base.smtp_settings = {
